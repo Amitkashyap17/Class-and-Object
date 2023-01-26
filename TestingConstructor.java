49f5a5a259c0aef7c5713096811573017f0fc8f2
Amit Kashyap <99862343+Amitkashyap17@users.noreply.github.com>
2023-01-26 22:06:20 +0530

    Add files via upload

---
+++ TestingConstructor.java
@@ -0,0 +1,11 @@
+package OPPS;
+
+public class TestingConstructor {
+
+    public static void main(String[] args) {
+
+        Student st1 = new Student();
+        Student st2= new Student(12);
+        Student st3 = new Student(12,"Amit");
+    }
+}
